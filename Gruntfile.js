16f46d99da01450ec31ed504e5f1635980b2d2ad
kristoferjoseph <kristoferjoseph@gmail.com>
2013-12-16 22:46:01 -0800

    Added travis ci

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
         simplemocha: {
-            options: {
-                ui: 'bdd',
-                reporter: 'nyan'
-            },
             all: {
                 src: ['test/*.test.js']
             }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 rework-namespace
 ================
-
+[![BuildStatus](https://travis-ci.org/kristoferjoseph/rework-namespace.png?branch=master)](https://travis-ci.org/kristoferjoseph/rework-namespace)
 Namespace plugin for rework
