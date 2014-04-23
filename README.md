bc7931070562c31ccd2f9002005df48727f84f82
Larry Davis <lawdavis@adobe.com>
2014-04-23 16:46:10 -0700

    Added options.not/options.only and associated documentation

--- README.md
+++ README.md
@@ -1,5 +1,75 @@
-rework-namespace
-================
-[![BuildStatus](https://travis-ci.org/kristoferjoseph/rework-namespace.png?branch=master)](https://travis-ci.org/kristoferjoseph/rework-namespace)
+# rework-namespace [![BuildStatus](https://travis-ci.org/kristoferjoseph/rework-namespace.png?branch=master)](https://travis-ci.org/kristoferjoseph/rework-namespace)
+> Namespace plugin for rework
 
-Namespace plugin for rework
+# Usage
+
+Pass the namespace as the first argument:
+
+```js
+var namespace = require('rework-namespace');
+
+var css = rework('.button { color: black; }')
+  .use(namespace('ns'))
+  .toString();
+```
+
+Results:
+
+```css
+.ns-button { color: black; }
+```
+
+## Options
+
+Pass an options object as the second argument.
+
+### options.not
+
+Don't prefix specific classes or classes that match a regex.
+
+```js
+var css = rework(inputCSS)
+  .use(namespace('ns', { not: [ /\.icon/, '.button-bar' ] }))
+  .toString();
+```
+
+### options.only
+
+Only prefix specific classes or classes that match a regex.
+
+```js
+var css = rework(inputCSS)
+  .use(namespace('ns', { only: [ /\.icon/, '.icon-group' ] }))
+  .toString();
+```
+
+## Examples
+
+### Prefix every class
+
+```js
+var css = rework(inputCSS)
+  .use(namespace('ns'))
+  .toString();
+```
+
+### Prefix every class except icon classes
+
+```js
+var css = rework(inputCSS)
+  .use(namespace('ns', {
+    not: /\.icon-/,
+  }))
+  .toString();
+```
+
+### Prefix all classes with "button" in them except .button itself
+
+```js
+var css = rework(inputCSS)
+  .use(namespace('ns', {
+    only: /button/,
+    not: '.button'
+  }))
+  .toString();
+```
--- index.js
+++ index.js
@@ -14,13 +14,53 @@
  *
  */
 
-module.exports = function(str) {
+// Returns true if a selector matches a test
+function selectorMatchesTest(selector, test) {
+    if (test instanceof RegExp) {
+        return test.exec(selector);
+    }
+    else if (typeof test === 'string') {
+        return selector === test;
+    }
+    return false;
+}
+
+// Returns true if a selector matches any test
+function selectorMatchesTests(selector, tests, fallback) {
+    // Convert strings to arrays
+    if (typeof tests === 'string') tests = [tests];
+
+    if (!tests || !tests.length) return fallback;
+
+    return tests.some(function(test) {
+        return selectorMatchesTest(selector, test);
+    });
+}
+
+module.exports = function(ns, options) {
+    options = options || {};
+
+    // Returns true if a selector matches any inclusion test
+    // Returns true if there are no inclusion test
+    function shouldIncludeSelector(selector) {
+        return selectorMatchesTests(selector, options.only, true);
+    }
+
+    // Returns true if a selector matches any exclusion test
+    // Returns false if there are no exclusion test
+    function shouldIgnoreSelector(selector) {
+        return selectorMatchesTests(selector, options.not, false);
+    }
+
     return function(style) {
+        // Do nothing if namespace was not specified
+        if (!ns || ns === '') return;
+
         style.rules = style.rules.map(function(rule) {
             if (!rule.selectors) return rule;
             rule.selectors = rule.selectors.map(function(selector) {
-                if (!str || str === '') return selector;
-                return selector.replace(/[.]/g, '.' + str + '-');
+                if (shouldIgnoreSelector(selector) || !shouldIncludeSelector(selector)) return selector;
+                return selector.replace(/\./g, '.' + ns + '-');
             });
             return rule;
         });
